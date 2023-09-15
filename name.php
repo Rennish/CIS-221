b53b7982b236fac7fb5b00eb8a16a25b81f3bf94
Rennish <rennishm@gmail.com>
2023-09-15 12:52:23 +0300

    first commit

---
+++ name.php
@@ -0,0 +1,47 @@
+<?php
+    echo "rennish" ;
+
+    echo "<br/>" ;
+
+
+
+    echo "<br/>" ;
+
+     $username = "fredrick odhiambo" ;
+     echo $username;
+
+     echo "<br/>" ;
+
+     $new_user = $username ;
+
+     echo $username ;
+
+     echo "<br/>" ;
+     $user = 2 ;
+
+     echo $user ;
+
+     $team= array("maid", "teacher", "servant", "electrician"); //declaring and definining an array
+
+     echo "<br/>" ;
+
+     echo $team[3];
+
+   // phpinfo(); 
+   // getting info of the executig parser. check things like its versiona and other configurations
+
+    /* this is how to a multiline comment in php. it works well when giving a summary of a php file. 
+     */
+
+     
+     $teams = array(array("joy", "peace", "love"), 
+                    array("twenty", "thirty", "fifty"), 
+                    array("shoes", "legs","hands")
+);
+
+      echo "<br/>" ;  
+
+
+      echo $teams[2][2];  // in reading a multidiensional array, we first read the row then column, and they all start at zero. 
+
+?>
